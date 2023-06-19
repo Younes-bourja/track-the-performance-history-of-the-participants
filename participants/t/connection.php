581136b7d60b15126dc68e235973e17a17c77d25
Younes-bourja <136999896+Younes-bourja@users.noreply.github.com>
2023-06-19 12:55:41 +0100

    v3

--- participants/t/connection.php
+++
@@ -1,6 +0,0 @@
-<?php $con = new mysqli("localhost","root","","ec");
-
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-  }
-  ?>
--- participants/t/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- Footer opened -->
-	<div class="main-footer ht-40">
-		<div class="container-fluid pd-t-0-f ht-100p">
-		<span>Copyright © 2023 <a href="#">Bouyoushop</a>. Build by bourja youness </span>
-		</div>
-	</div>
-<!-- Footer closed -->
